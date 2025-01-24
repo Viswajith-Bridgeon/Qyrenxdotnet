c0fdb2b1fc615277c56a147b3af9446f6dc1cb35
Arunkanakkillam <arungeo70@gmail.com>
2025-01-24 10:06:25 +0530

    some change

--- Busines/DTOs/Deliverypersons/DeliveryPersonRegDto.cs
+++ Busines/DTOs/Deliverypersons/DeliveryPersonRegDto.cs
@@ -11,7 +11,7 @@ namespace Qyrenx.Business.Models.DTOs.Deliverypersons
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        public int Mobile { get; set; }
+        public string Mobile { get; set; }
         [Required(ErrorMessage = "Password is required.")]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
         ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
